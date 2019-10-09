1495773c961ccad99fa4a7fe6c601fe497de2553
lixiaofeng <m18210682571@163.com>
2019-10-09 20:27:58 +0800

    update

--- crm_parent/crm_service/src/main/java/com/dt/crm/service/CrmServiceApplication.java
+++ crm_parent/crm_service/src/main/java/com/dt/crm/service/CrmServiceApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("com.dt.mapper*")
+@ComponentScan(value = {"com.dt"})
 public class CrmServiceApplication {
 
     public static void main(String[] args) {
